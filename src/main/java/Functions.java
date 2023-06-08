7e2b009cd231db566e1d377b7754b4b5efbf9573
Ben-21 <bs@schaefer-inet.de>
2023-06-08 16:07:16 +0200

    commit

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -1,2 +1,39 @@
+import java.sql.Array;
+
 public class Functions {
+
+
+
+    public  int find(int[] arrayOfIntegers){
+        int result = 0;
+        int oddCounter = 0;
+        int evenCounter = 0;
+        int[] oddArray = new int[arrayOfIntegers.length];
+        int[] evenArray = new int[arrayOfIntegers.length];
+
+
+        for (int i = 0; i < arrayOfIntegers.length; i++){
+            if (arrayOfIntegers[i] % 2 != 0){
+                oddArray[i] = arrayOfIntegers[i];
+                oddCounter++;
+            }
+
+            if (arrayOfIntegers[i] % 2 == 0){
+                evenArray[i] = arrayOfIntegers[i];
+                evenCounter++;
+            }
+        }
+
+        if (oddCounter < evenCounter){
+            result = oddArray[evenCounter];
+        } else {
+            result = evenArray[oddCounter];
+        }
+
+
+
+        return result;
+
+
+    }
 }
--- src/test/java/FunctionsTest.java
+++ src/test/java/FunctionsTest.java
@@ -1,7 +1,30 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FunctionsTest {
 
+    @Test
+    void findOddNumbers(){
 
+    int[] inputArray = {1, 2, 4,20};
+    Functions functions = new Functions();
+
+    int actual = functions.find(inputArray);
+    int expected = 1;
+
+
+        Assertions.assertEquals(expected, actual);
+}@Test
+    void findEvenNumbers(){
+
+    int[] inputArray = {1, 3, 11,20};
+    Functions functions = new Functions();
+
+    int actual = functions.find(inputArray);
+    int expected = 20;
+
+
+        Assertions.assertEquals(expected, actual);
+}
 
 }
